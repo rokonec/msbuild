8f2e88745956dc649afe16d21e6a68421c9d373c
Ladi Prosek <laprosek@microsoft.com>
2020-09-07 14:11:05 +0200

    Add the missing StringTools.cs

---
+++ src/StringTools/StringTools.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+
+namespace StringTools
+{
+    /// <summary>
+    /// A callback type to register with the library. The library will invoke this callback in registration order until
+    /// true is returned. If no callback returns true, the library will fall back to its default interning logic.
+    /// </summary>
+    /// <param name="internableString">The string to be interned.</param>
+    /// <param name="result">The interned string. Set to null and return true if the string should not be interned.</param>
+    /// <returns>True if the <paramref name="internableString" /> was handled by the callback.</returns>
+    public delegate bool TryInternStringDelegate(ref InternableString internableString, out string result);
+
+    public static class StringTools
+    {
+        private static volatile TryInternStringDelegate[] s_internStringCallbacks = new TryInternStringDelegate[0];
+
+        private static object s_locker = new object();
+
+        public static void EnableDiagnostics()
+        {
+            //
+        }
+
+        public static string CreateDiagnosticReport()
+        {
+            return string.Empty;
+        }
+
+        public static void RegisterStringInterningCallback(TryInternStringDelegate callback)
+        {
+            lock (s_locker)
+            {
+                if (s_internStringCallbacks.Any(existingCallback => existingCallback == callback))
+                {
+                    TryInternStringDelegate[] newInternStringCallback = new TryInternStringDelegate[s_internStringCallbacks.Length + 1];
+                    s_internStringCallbacks.CopyTo(newInternStringCallback, 0);
+                    newInternStringCallback[s_internStringCallbacks.Length] = callback;
+
+                    s_internStringCallbacks = newInternStringCallback;
+                }
+            }
+        }
+
+        public static void UnregisterStringInterningCallback(TryInternStringDelegate callback)
+        {
+            lock (s_locker)
+            {
+                s_internStringCallbacks = s_internStringCallbacks.Where(existingCallback => existingCallback == callback).ToArray();
+            }
+        }
+
+        internal static bool CallStringInterningCallbacks(ref InternableString candidate, out string interned)
+        {
+            TryInternStringDelegate[] callbacks = s_internStringCallbacks;
+            foreach (TryInternStringDelegate callback in callbacks)
+            {
+                if (callback(ref candidate, out interned))
+                {
+                    return true;
+                }
+            }
+            interned = null;
+            return false;
+        }
+    }
+}
